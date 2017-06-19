46b996b999cea272cd3ed5b0141acb6a0af721c0
Rutvij07 <rutvijdoshi07@gmail.com>
2017-06-19 10:15:00 +0530

    v0.1 Angular CRUD Base
    This commit consists of base application of Bootstrap + AngularJS to do basic CRUD operations.

---
+++ view/controller/crud.js
@@ -0,0 +1,26 @@
+var app = angular.module('myApp',[]);
+app.controller('myCtrl',function($scope){
+
+$scope.users=[
+  {firstname:'Rutvij',lastname:'Doshi',email:'rutvijdoshi07@gmail.com'}
+];
+
+$scope.saveUser = function(){
+
+$scope.users.push($scope.newUser);
+$scope.newUser={};
+};
+
+$scope.editUser = function(user){
+  $scope.clickedUser=user;
+};
+
+$scope.updateUser=function(){
+alert('Updated');
+};
+
+$scope.deleteUser=function(){
+$scope.users.splice($scope.users.indexOf($scope.clickedUser),1);
+};
+
+});
